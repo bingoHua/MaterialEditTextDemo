f705b9a6072d47cddb60925b50a7de3e8ae71788
lizehua <lizehua@insta360.com>
2021-03-01 22:39:50 +0800

    first commit

---
+++ app/src/main/java/com/insta360/materialedittextdemo/MaterialEditTextDemo.kt
@@ -0,0 +1,16 @@
+package com.insta360.materialedittextdemo
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+
+class MaterialEditTextDemo(context: Context, attrs: AttributeSet) :
+    AppCompatEditText(context, attrs) {
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+    }
+
+}
